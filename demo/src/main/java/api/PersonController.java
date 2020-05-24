488ccbbf3af45796756d0536a713e92f8bbeabfc
jacobstac <jacob.benjamin.stachowicz@gmail.com>
2020-05-24 16:52:50 +0200

    create an endpoint for POST request

--- demo/src/main/java/api/PersonController.java
+++ demo/src/main/java/api/PersonController.java
@@ -1,18 +1,28 @@
 package api;
 
 import model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import service.PersonService;
 
-public class PersonController {
+@RequestMapping("api/v1/person")
+@RestController
+public class  PersonController {
 
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
-    public void addPerson(Person person) {
+    @PostMapping
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
-
     }
 }
+
